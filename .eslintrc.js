fa94dbe2230237f1958f6091a6ba5e543e174abb
Yash Maheshwari <yash.maheshwari@hotwax.co>
2022-06-01 15:37:16 +0530

    Implemented: eslint configuration and type script configuration(#2ebeamz)

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
