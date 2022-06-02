aee53f22a38ae3dd402b584cf10227adec768b2f
Ravindra Purohit <ravindrapurohit2011@gmail.com>
2022-06-02 14:56:25 +0530

    Updated: Products types and returned reponse in grouped format (#2ebfxn4)

--- src/product/index.ts
+++ src/product/index.ts
@@ -9,26 +9,41 @@ export async function getProductDetails (productId: string): Promise<any> {
     }
   }
 
+  const query = {
+    "json": {
+      "params": {
+        "group": true,
+        "group.field": `groupId`,
+        "group.limit": 10000,
+        "group.ngroups": true
+      } as any,
+      "query": "*:*",
+      "filter": `docType: PRODUCT AND productId: ${productId}`
+    }
+  }
+
   try {
     const resp = await api({
       url: 'solr-query',
       method: 'post',
-      data: payload
+      data: query
     })
 
     if (resp?.status == 200) {
 
-      const productDetails = resp.data.response.docs[0];
+      const group = resp.data.grouped.groupId.groups[0]
+      const productDetails = group.doclist.docs[0]
       const imageUrls = productDetails.additionalImageUrls
       imageUrls.push(productDetails.mainImageUrl)
 
       const product: Product = {
-        productId: productDetails.productId,
-        productName: productDetails.productName, 
+        id: productDetails.productId,
+        name: productDetails.productName, 
         description: productDetails.description,
         brand: productDetails.brandName,
+        price: productDetails.BASE_PRICE_PURCHASE_USD_NN_STORE_GROUP_price,
         sku: productDetails.sku,
-        identifications: productDetails.goodIdentifications, 
+        identifications: productDetails.goodIdentifications,
         /** An array containing assets like images and videos */
         assets: imageUrls,
         mainImage: productDetails.mainImageUrl,
