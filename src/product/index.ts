c393b831159fffa1951c8763aef2ea67ca4c8dc8
Ravindra Purohit <ravindrapurohit2011@gmail.com>
2022-06-02 15:20:35 +0530

    Improved: Now other variants are stored in form of group and removed unwanted code (#2ebfxn4)

--- src/product/index.ts
+++ src/product/index.ts
@@ -2,20 +2,14 @@ import api from '../../api'
 import { Product } from '../../types/Product';
 
 export async function getProductDetails (productId: string): Promise<any> {
-  const payload = {
-    "json": {
-      "query": "*:*",
-      "filter": `docType: PRODUCT AND productId: ${productId}`
-    }
-  }
-
   const query = {
     "json": {
       "params": {
         "group": true,
         "group.field": `groupId`,
         "group.limit": 10000,
-        "group.ngroups": true
+        "group.ngroups": true,
+        "rows":10
       } as any,
       "query": "*:*",
       "filter": `docType: PRODUCT AND productId: ${productId}`
@@ -29,12 +23,33 @@ export async function getProductDetails (productId: string): Promise<any> {
       data: query
     })
 
-    if (resp?.status == 200) {
+    if (resp?.status == 200 && resp.data?.grouped?.groupId?.groups?.length > 0) {
 
       const group = resp.data.grouped.groupId.groups[0]
       const productDetails = group.doclist.docs[0]
       const imageUrls = productDetails.additionalImageUrls
       imageUrls.push(productDetails.mainImageUrl)
+
+      const variantsGroup: Array<any> = group.doclist.docs.map((variant: any) => {
+        variant = {
+          id: variant.productId,
+          name: variant.productName, 
+          description: variant.description,
+          brand: variant.brandName,
+          price: variant.BASE_PRICE_PURCHASE_USD_NN_STORE_GROUP_price,
+          sku: variant.sku,
+          identifications: variant.goodIdentifications,
+          /** An array containing assets like images and videos */
+          assets: imageUrls,
+          mainImage: variant.mainImageUrl,
+          parentProductId: variant.parentProductName,
+          type: variant.productTypeId, // TODO: need to fetch the type description
+          category: variant.productCategoryNames,
+          feature: variant.productFeatures,
+        }
+
+        return variant
+      })
 
       const product: Product = {
         id: productDetails.productId,
@@ -51,7 +66,7 @@ export async function getProductDetails (productId: string): Promise<any> {
         type: productDetails.productTypeId, // TODO: need to fetch the type description
         category: productDetails.productCategoryNames,
         feature: productDetails.productFeatures,
-        variants: productDetails.variantProductId // TODO: need to fetch the variant details
+        variants: variantsGroup // TODO: need to fetch the variant details
       }
 
       return product;
