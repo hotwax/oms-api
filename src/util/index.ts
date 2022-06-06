df97f3b452e0a16e6f74a1387cc2c517eeae2372
Yash Maheshwari <yash.maheshwari@hotwax.co>
2022-06-06 12:18:09 +0530

    Implemented: method to check whether the resp contains a code error and returns true if the resp has code equal to error(#2ebeamz)

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,7 @@
 export async function hasError(resp: any) {
   return !!resp.data._ERROR_MESSAGE_ || !!resp.data._ERROR_MESSAGE_LIST_ || resp.data.error;
 }
+
+export async function isError(resp: any): Promise<boolean> {
+  return resp.code === 'error'
+}
