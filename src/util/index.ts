30f2fc65e5844c731409d06a459ec74bff29af43
Yash Maheshwari <yash.maheshwari@hotwax.co>
2022-06-06 12:27:00 +0530

    Improved: the return type of isError method to boolean and made the function as sync(#2ebeamz)

--- src/util/index.ts
+++ src/util/index.ts
@@ -2,6 +2,6 @@ export async function hasError(resp: any) {
   return !!resp.data._ERROR_MESSAGE_ || !!resp.data._ERROR_MESSAGE_LIST_ || resp.data.error;
 }
 
-export async function isError(resp: any): Promise<boolean> {
+export function isError(resp: any): boolean {
   return resp.code === 'error'
 }
