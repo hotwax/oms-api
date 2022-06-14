bb17033b2916faf17c50b3dd5a3b9d12e9fe03e3
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-06-14 17:32:12 +0530

    Improved: the types in the package to define all the dependent schemas for the order(#2fkjqd3)

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,11 @@
-import { Order } from './Order';
+import { CommunicationEvent } from "./CommunicationEvent";
+import { ContactMech } from "./ContactMech";
+import { Enumeration } from "./Enumeration";
+import { Geo } from "./Geo";
+import { Order, OrderItem, OrderPart } from "./Order";
+import { Party, PartyIdentification } from "./Party";
+import { Status } from "./Status";
+import { Uom } from "./Uom";
 
 export const events = {
   'UNAUTHORIZED': 'unauthorized',
@@ -14,5 +21,15 @@ export interface Response {
 }
 
 export {
-  Order
+  CommunicationEvent,
+  ContactMech,
+  Enumeration,
+  Geo,
+  PartyIdentification,
+  Order,
+  OrderItem,
+  OrderPart,
+  Party,
+  Status,
+  Uom
 }
