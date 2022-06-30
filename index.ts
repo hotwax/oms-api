f1ed3fb2de20e149d0283dff3ffd00b7bdbd5c40
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-06-30 18:57:14 +0530

    Improved: the eslint rules to disable no-use-before-define, removed async from the hasError function and exported the methods from the api(#2h1fjk7)

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 import { getOrderDetails, updateOrderStatus } from './src/order'
 import { events } from './src/types'
+import { fetchProducts } from './src/product'
+import { init, updateToken, updateInstanceUrl } from './api'
 
 export {
   getOrderDetails,
   updateOrderStatus,
+  fetchProducts,
+  init,
+  updateToken,
+  updateInstanceUrl,
   events
 }
