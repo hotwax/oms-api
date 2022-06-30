ec9c307d080a5ff06d5200389d011635d40907d5
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-06-30 18:59:31 +0530

    Implemented: mapping rules for product tranformation(#2h1fjk7)

---
+++ src/mappings/product.ts
@@ -0,0 +1,56 @@
+export const productsTransformRule = {
+  item: {
+    productId: "productId",
+    pseudoId: "internalName",
+    productName: "productName",
+    salesIntroductionDate: "introductionDate",
+    toAssocs: "variantProductIds",
+    contents: [{
+      productContentId: "main",
+      productId: "productId",
+      contentLocation: "mainImageUrl"
+    },{
+      productContentId: "",
+      productId: "productId",
+      contentLocation: "additionalImageUrls"
+    }],
+    features: {
+      hierarchy: "featureHierarchy",
+      features: "productFeatures",
+      ids: "productFeatureIds",
+      productId: "productId"
+    },
+    categories: "productCategoryNames"
+  },
+  operate: [{
+    run: function(val: any) {
+      return val.ids.map((id: any, index: number) => ({
+        "productId": val.productId,
+        "productFeatureId": id,
+        "feature": {
+          "productFeatureId": id,
+          "productFeatureTypeEnumId": val.hierarchy.find((feature: any) => feature.startsWith(`1/${val.features[index].split("/")[0].toUpperCase()}`)).split("/")[1],
+          "description": val.features[index].split("/")[1]
+        }
+      }))
+    },
+    on: "features"
+  }, {
+    run: function(val: Array<string>) {
+      if (val) {
+        return val.map(id => ({"toProductId": id}))
+      }
+    },
+    on: "toAssocs"
+  }, {
+    run: function(categories: Array<string>) {
+      if (categories) {
+        return categories.map(category => ({"category": { "categoryName": category }}))
+      }
+    },
+    on: "categories"
+  }],
+  defaults: {
+    "productContentId": "mainImage"
+  }
+}
