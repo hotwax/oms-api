4fbdfd785779900c0674ee7a5342b8897014ad9f
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-01-11 14:31:04 +0530

    Improved: the return from function to use resolve reject pattern(#2v1pmpv)

--- src/modules/stock/index.ts
+++ src/modules/stock/index.ts
@@ -5,8 +5,6 @@ import { transform } from "node-json-transform";
 import { stockTransformRule } from "@/mappings/stock";
 
 async function fetchProductsStockAsPerFacility(productIds: Array<string>, facilityId?: string): Promise<Array<Stock> | Response> {
-  let response: Array<Stock> | Response
-
   // There is a limitation at API level to handle only 100 records
   // but as we will always fetch data for the fetched records which will be as per the viewSize
   // assuming that the value will never be 100 to show
@@ -37,23 +35,21 @@ async function fetchProductsStockAsPerFacility(productIds: Array<string>, facili
     if (resp.status === 200 && !hasError(resp) && resp.data.count > 0) {
       const productsStock: Array<Stock> = transform(resp.data.docs, stockTransformRule)
 
-      response = productsStock
+      return Promise.resolve(productsStock)
     } else {
-      response = {
+      return Promise.reject({
         code: "error",
         message: "Unable to find the stock for products",
         serverResponse: ""
-      }
+      })
     }
   } catch(err) {
-    response = {
+    return Promise.reject({
       code: "error",
       message: "Something went wrong",
       serverResponse: err
-    }
+    })
   }
-
-  return response;
 }
 
 export { fetchProductsStockAsPerFacility }
