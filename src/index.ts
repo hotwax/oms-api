75658aebfeb920ba9051c0aa9ca97bf1541757b4
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-02-03 15:04:29 +0530

    Improved: code to expose the api and client method from oms api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { events, Product, Response, Stock, Order, OrderItem, OrderPart, OPERATOR, User } from '@/types'
-import { init, resetConfig, updateToken, updateInstanceUrl } from '@/api'
+import api, { client, init, resetConfig, updateToken, updateInstanceUrl } from '@/api'
 import { isError } from '@/util'
 import { fetchProducts, fetchProductsGroupedBy, fetchProductsGroupedByParent, fetchProductsStock, fetchProductsStockAtFacility, getOrderDetails, getProfile, updateOrderStatus } from '@/modules'
 
 export {
+  api,
+  client,
   getOrderDetails,
   updateOrderStatus,
   fetchProducts,
