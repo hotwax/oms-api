e7d8964413601c60a346b6da6898613d474b23ab
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-09-12 15:13:54 +0530

    Implemented: support for logout api

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { events, Product, Response, Stock, Order, OrderItem, OrderPart, OPERATOR, User } from './types'
 import api, { client, getConfig, init, initialise, resetConfig, updateToken, updateInstanceUrl } from './api'
 import { hasError, isError } from './util'
-import { fetchProducts, fetchProductsGroupedBy, fetchProductsGroupedByParent, fetchProductsStock, fetchProductsStockAtFacility, getOrderDetails, getProductIdentificationPref, getProfile, setProductIdentificationPref, updateOrderStatus } from './modules'
+import { fetchProducts, fetchProductsGroupedBy, fetchProductsGroupedByParent, fetchProductsStock, fetchProductsStockAtFacility, getOrderDetails, getProductIdentificationPref, getProfile, logout, setProductIdentificationPref, updateOrderStatus } from './modules'
 
 export {
   api,
@@ -12,6 +12,7 @@ export {
   fetchProductsGroupedBy,
   fetchProductsGroupedByParent,
   getConfig,
+  logout,
   hasError,
   init,
   initialise,
